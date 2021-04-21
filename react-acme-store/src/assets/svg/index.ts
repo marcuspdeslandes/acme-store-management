bd289e019ce4d5295b64359f24ab8989bdb85d10
Marcus Deslandes <marcus.martins@celfocus.com>
2021-04-21 09:04:13 +0100

    clear repository

--- react-acme-store/src/assets/svg/index.ts
+++
@@ -1,2 +0,0 @@
-export { ReactComponent as ChevronLeft } from './chevron-left.svg';
-export { ReactComponent as DoubleArrows } from './double-arrows.svg';
--- vue-acme-store/src/http-common.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:8082/acme/store/v1/persist/",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
